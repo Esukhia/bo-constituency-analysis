8a24302249ed7da2cf4b674ad74b982244ffe3f3
drupchen <hhdrupchen@gmail.com>
2019-10-15 17:44:57 +0200

    oops

--- syntactic_analysis/__init__.py
+++ syntactic_analysis/__init__.py
@@ -1,2 +1,3 @@
 from .prepare import prepare_file
 from .analysis import generate_analysis, analyze_constituency
+from .spreadsheet_utils import translate_trees, translate_tsv_dir, xlsx_to_tsv, tsv_to_xlsx
